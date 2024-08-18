c7d62b858100a97e7b850b788e58c5450032df6c
Rodrigo_Kelven <92493747+Rodrigo-Kelven@users.noreply.github.com>
2024-08-18 10:07:31 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ Usando a função " register_blueprint " e passando no parametro a função que 
 
 
 app.register_blueprint(home_rotas)
-app.register_blueprint(clientes_rotas, url_prefix='/clientes') # este | url_prefix='' | serve basicamente para setar uma "restrição" em todas as urls da rota /clientes.
+app.register_blueprint(clientes_rotas, url_prefix='/clientes') """ este | url_prefix='' | serve basicamente para setar uma "restrição" em todas as urls da rota /clientes.
 Exemplo: para acessar a rota /new, tenho que acessar a rota /clientes . só assim posso acessar a rota /clientes/new
+"""
 
 # Execução do Flask em modo debug= True
 app.run(debug=True)
